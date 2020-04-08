379f1a91762a16174627453bd8d60308646fbc8e
Gregory Casamento <greg.casamento@gmail.com>
2020-04-08 06:08:36 -0400

    Initial Commit

---
+++ NSSwitch_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSSwitch_test
+//
+//  Created by Gregory Casamento on 4/8/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
